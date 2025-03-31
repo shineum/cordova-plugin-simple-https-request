cc161ff1b2c0546bc4181add16e4da38feba700a
Sungwon Um <shineum@gmail.com>
2025-03-30 21:27:41 -0700

    update options

--- www/simplehttpsrequest.js
+++ www/simplehttpsrequest.js
@@ -11,16 +11,14 @@ function CordovaPluginSimpleHttpsRequest() {
 // pOptions = {"method": "GET, "params": {}, "headers": {}, "data": "", "isHttpRequest": false, "isResDataBinary", false, "ignoreSSL": false}
 CordovaPluginSimpleHttpsRequest.prototype.request = function (pUrl, pOptions, pCallback) {
       let tOptions = pOptions || {};
-      if (!tOptions.method) {
-            tOptions.method = "GET";
-      } else {
-            tOptions.method = tOptions.method.trim().toUpperCase();
-      }
-      let tData = (typeof tOptions.data === 'object') ? JSON.stringify(tOptions.data) : (tOptions.data || "");
+      let tMethod = (tOptions.method) ? tOptions.method.trim().toUpperCase() : "GET";
+      let tParams = tOptions.params || {};
+      let tHeaders = tOptions.headers || {};
+      let tData = (tOptions.data && typeof tOptions.data === 'object') ? JSON.stringify(tOptions.data) : (tOptions.data || "");
       let tReqProtocol = (tOptions.isHttpRequest || false) ? "httpRequest" : "httpsRequest";
       let tIsResDataBinary = tOptions.isResDataBinary || false;
       let tIgnoreCert = tOptions.ignoreSSL || false;
-      exec((pRet)=>{pCallback(JSON.parse(pRet));}, null, 'SimpleHttpsRequest', tReqProtocol, [pUrl, tOptions.method || "", tOptions.params || {}, tOptions.headers || {}, tData, tIsResDataBinary, tIgnoreCert]);
+      exec((pRet)=>{pCallback(JSON.parse(pRet));}, null, 'SimpleHttpsRequest', tReqProtocol, [pUrl, tMethod, tParams, tHeaders, tData, tIsResDataBinary, tIgnoreCert]);
 };
 CordovaPluginSimpleHttpsRequest.prototype.get = function (pUrl, pOptions, pCallback) {
       let tOptions = pOptions || {};
